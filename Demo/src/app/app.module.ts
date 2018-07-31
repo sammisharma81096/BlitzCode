836e78a55ef6b2ffad7a0438bc063e47d92fd2d1
Sammi Sharma <sammisharma81096@gmail.com>
2018-07-31 16:08:42 +0530

    Task component added and Router configured

--- Demo/src/app/app.module.ts
+++ Demo/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'task', component: TaskComponent },
+  { path: '', redirectTo: '/task', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
